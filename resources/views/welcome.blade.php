927d87652187dfb5dcc78cf74814d64dec3937d0
Adams899591 <usmanadams551@gmail.com>
2026-05-27 06:11:53 -0700

    g

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,8 @@
 <body>
     
     <h1>THis is my portifolio welcome page</h1>
+        <h1>THis is my portifolio welcome page</h1>
+     <h1>THis is my portifolio welcome page</h1>
      <h1>THis is my portifolio welcome page</h1>
      {{-- --}}
      <iframe src="" frameborder="0"></iframe>
